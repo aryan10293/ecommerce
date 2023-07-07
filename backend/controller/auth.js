f546335f6e5f38c41c5000a136f4602d57ca9279
aondrej ryan <aondrejryan11@gmail.com>
2023-07-07 15:14:07 -0700

    might have an idea that will save time

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -118,7 +118,7 @@ module.exports = {
       },
       checkUser:  async (req,res) => {
         console.log(req.params.id)
-        const getUser = User.findById({_id: req.params.id})
+        const getUser = User.find({_id: req.params.id})
         console.log(getUser)
         //res.send( getUser )
     }
