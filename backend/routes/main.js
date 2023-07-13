914a5d9da4e415491fee0c0a3674ecca990e82f7
aondrej ryan <aondrejryan11@gmail.com>
2023-07-12 19:54:59 -0700

    Update .gitignore

---
+++ backend/routes/main.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controller/auth")
+const crudController = require("../controller/crud")
+const nodemailer = require("nodemailer");
+const passport = require('passport')
+const cors = require('cors')
+const { ensureAuth, ensureGuest } = require("../middleware/auth")
+
+router.post('/createaccount', authController.postCreateAccount)
+router.post('/login', authController.postLogin)
+
+router.get("/logout", authController.logout)
+router.get("/getLoginUser/:id", authController.checkUser)
+router.get('/wish/:id', crudController.getWishList)
+router.get('/cart/:id', crudController.getCart)
+
+router.put('/wish/:id', crudController.addWish)
+router.put('/cart/:id', crudController.addCart)
+
+router.post('/confirmorder/:id', crudController.confirmOrder)
+router.options('/deletecart', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  res.sendStatus(204); // Send a successful response with status code 204 (No Content)
+})
+router.delete('/deletecart/:id', crudController.whyWontItWork)
+//router.delete('/wish/:id', crudController.deleteWish)
+router.delete('/clearcart/:id', crudController.clearCart)
+module.exports = router;
